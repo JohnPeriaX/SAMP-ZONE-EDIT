80c6b7ca143ca572ef9ea4796e23ca1c5b4bfa9e
JohnPeriaX <johnperia.job@gmail.com>
2025-11-07 21:02:43 +0700

    Add files via upload

---
+++ Grimrandomer-samp-zone-editor-05b6d5f46edc/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SAMP Zone Editor")]
+[assembly: AssemblyDescription("SAMP Zone Editor by Grimrandomer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Grimrandomer Productions")]
+[assembly: AssemblyProduct("SAMP Zone Editor")]
+[assembly: AssemblyCopyright("Copyright © Grimrandomer.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c41c829b-9d71-487e-bf4e-cc00f3a9860d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
